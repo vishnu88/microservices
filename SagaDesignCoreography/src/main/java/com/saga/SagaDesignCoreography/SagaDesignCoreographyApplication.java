09d33e850fb76bb48defb4f14af2966dcdb5d260
Mayank <vishnu15788@gmail.com>
2024-04-09 11:42:05 +0530

    initial commits

---
+++ SagaDesignCoreography/src/main/java/com/saga/SagaDesignCoreography/SagaDesignCoreographyApplication.java
@@ -0,0 +1,13 @@
+package com.saga.SagaDesignCoreography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaDesignCoreographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaDesignCoreographyApplication.class, args);
+	}
+
+}
---
+++ SagaDesignCoreography/src/test/java/com/saga/SagaDesignCoreography/SagaDesignCoreographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.SagaDesignCoreography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaDesignCoreographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
